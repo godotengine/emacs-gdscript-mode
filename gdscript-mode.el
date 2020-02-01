06afaea94041f0057729e74d7aaa2d4058086408
Nathan Lovato <nathan@gdquest.com>
2020-02-01 09:03:52 -0600

    Add package header

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -1,13 +1,41 @@
-;;; gdscript-mode.el --- Major mode to edit Godot GDScript code.
-;; URL: https://github.com/GDQuest/emacs-gdscript-mode
-;; Version: 202001141-git
+;;; gdscript-mode.el --- GDScript mode                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 GDQuest, Free Software Foundation, Inc.
+
+;; Author: Nathan Lovato <nathan@gdquest.com>, Fabián E. Gallina <fgallina@gnu.org>
+;; URL: https://github.com/GDQuest/emacs-gdscript-mode/
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.0"))
+;; Maintainer: nathan@gdquest.com
+;; Created: Jan 2020
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Support for the Godot GDScript programming language in Emacs.
+
+;;; Code:
+
 (add-to-list 'auto-mode-alist '("\\.gd\\'" . gdscript-mode))
 (add-to-list 'auto-mode-alist '("\\.tscn\\'" . conf-toml-mode))
 
 (defgroup gdscript nil
   "GDScript language support for Emacs."
   :group 'languages
-  :version "27"
+  :version "26"
   :link '(emacs-commentary-link "gdscript"))
 
 (defvar gdscript-mode-map (let ((map (make-sparse-keymap)))
