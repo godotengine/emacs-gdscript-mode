b5eb183d4447fbac832026a125a85524ead83419
Nathan Lovato <nathan@gdquest.com>
2020-02-14 10:18:37 -0600

    Revert "Load file gdscript-rx.el in Emacs 26 and before"
    This reverts commit 80260033231146184588b8ebde04bd576cb08cbd.
    I'm having an error that the file gdscript-rx can't be found in Spacemacs, but
    I'm not sure why.

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -42,7 +42,6 @@
 ;; HACK To make this work in emacs-26 and below
 ;; This is the definition ripped straight from emacs-27
 (if (version< emacs-version "27")
-    (load-file "gdscript-rx.el")
     (require 'gdscript-rx))
 
 (defvar gdscript-mode-map (let ((map (make-sparse-keymap)))
