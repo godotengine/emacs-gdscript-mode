fb215bb83b286047e3d129b9ff45063effb40297
Nathan Lovato <nathan@gdquest.com>
2020-03-01 06:56:48 -0600

    Fix linter error in gdscript-mode body
    Use setq-local for hs-special-modes-alist

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -145,16 +145,14 @@ the last command event was a string delimiter."
 
   (add-hook 'which-func-functions #'gdscript-info-current-defun nil t)
 
-  (with-eval-after-load 'hideshow (add-to-list
-                                   'hs-special-modes-alist
-                                   '(gdscript-mode
-                                     "\\s-*\\_<\\(?:func\\|class\\)\\_>"
-                                     ;; use the empty string as end regexp so it doesn't default to
-                                     ;; "\\s)".  this way parens at end of defun are properly hidden.
-                                     ""
-                                     "#"
-                                     gdscript-hideshow-forward-sexp-function
-                                     nil)))
+  (setq-local hs-special-modes-alist '(gdscript-mode
+                                       "\\s-*\\_<\\(?:func\\|class\\)\\_>"
+                                       ;; use the empty string as end regexp so it doesn't default to
+                                       ;; "\\s)".  this way parens at end of defun are properly hidden.
+                                       ""
+                                       "#"
+                                       gdscript-hideshow-forward-sexp-function
+                                       nil))
 
   (setq-local outline-regexp
               (gdscript-rx (* space) block-start))
