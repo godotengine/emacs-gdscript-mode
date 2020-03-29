beac587761a480e83deb4d088824d2a41e35b832
Franco Eusébio Garcia <francoegarcia@outlook.com>
2020-03-29 13:57:31 -0300

    Add commands to open and run files in Godot
    Closes #11 and #13.

--- gdscript-mode.el
+++ gdscript-mode.el
@@ -57,6 +57,14 @@
                             (define-key map "\177" 'gdscript-indent-dedent-line-backspace)
                             (define-key map (kbd "<backtab>") 'gdscript-indent-dedent-line)
                             (define-key map (kbd "\t") 'company-complete)
+                            ;; Run in Godot.
+                            (define-key map "\C-c\C-g" 'gdscript--run-godot-editor)
+                            (define-key map "\C-c\C-p" 'gdscript--run-project-in-godot)
+                            (define-key map "\C-c\C-s" 'gdscript--run-current-scene-in-godot)
+                            (define-key map "\C-c\C-e" 'gdscript--edit-current-scene-in-godot)
+                            (define-key map "\C-c\C-r" 'gdscript--run-current-script-in-godot)
+                            (define-key map "\C-c\C-dp" 'gdscript--run-project-in-godot-debug-mode)
+                            (define-key map "\C-c\C-ds" 'gdscript--run-current-scene-in-godot-debug-mode)
                             map)
   "Keymap for `gdscript-mode'.")
 
