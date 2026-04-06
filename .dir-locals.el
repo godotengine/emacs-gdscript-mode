940834b8218c9513762ae9332d240938e266cce0
John Charman <102669158+charjr@users.noreply.github.com>
2026-04-06 19:00:45 +0100

    Add Directory Local Variables for indentation

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode . nil))
+ (makefile-mode
+  (indent-tabs-mode . t)))
